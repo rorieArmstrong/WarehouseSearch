2ffb18f1e776284bd5b776d541fb6a13cab992a3
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-08-18 17:01:38 +0100

    Search field with state

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [querry, setQuerry] = useState('')
+
+  const search = () => {
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ height: 40,width: 80, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setQuerry(text)}
+        value={querry}
+      />
+      <Button
+        onPress={search}
+        title="Search"
+        color="#841584"
+        accessibilityLabel="Start Search"
+      />
       <StatusBar style="auto" />
     </View>
   );
